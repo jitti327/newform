5b7da56f7882caaefc693de0d51d5e42bf5f83c0
Jitti327 <jitti327@gmail.com>
2018-10-12 17:05:40 +0530

    New Watch made

--- stopwatch1.php
+++ stopwatch1.php
@@ -1,75 +1,115 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<title>Stop Watch With Timer</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <script src="js/jquery.min.js"></script>  
   <script src="js/bootstrap.min.js"></script>
+  <style type="text/css">
 
-  <style>
-    body{
-      margin-top: 40px;
-      text-align: center;
-    }
+  	.container{	
+  		width   				  : 500px;
+  		margin-top 			  : 40px;  		
+  		min-height 			  : 300px;
+  		border 					  : 1px solid grey;
+  		background-color  : rgb(229, 232, 232);
+  	}
 
-    #output{
-      width: 100%;
-    }
+  	.stopwatch{
+  		width         		: 50%;
+  		float  						: left;
+  		height  					: 40px;
+  		border  					: none;
+  		background-color  : rgb(174, 214, 241);
+  	}
 
+  	.timer{
+  		width  					  : 49%;
+  		float  					  : right;
+  		height  				  : 40px;
+  		border  				  : none;  		
+  		background-color  : rgb(163, 228, 215);
+  	}
+
+  	#output{
+  		text-align     	  : center;
+  	}
     b{
-      font-size: 40px;
-      font-family: sans-serif;
+      font-size 	 			: 40px;
+      font-family  			: sans-serif;
     }
 
     a{
-      font-size: 26px;
-      font-family: sans-serif;
+      font-size 				: 26px;
+      font-family 			: sans-serif;
     }
-
-    #myProgress {
-      width: 100%;
-      height: 3px;
-      position: relative;
-      background-color: #ddd;
+    .st{
+  		width  					  : 50%;
+  		float 					  : left;
+  		height 					  : 30px;
+  		border   				  : none;
+  		background-color  : rgb(215, 189, 226);
     }
-
-    #myBar {
-      background-color: #4CAF50;
-      width: 1%;
-      height: 3px;
-      position: absolute;
+    .re{
+  		width  						: 49%;
+  		float  						: right;
+  		height 					  : 30px;
+  		border  					: none;
+  		background-color 	: rgb(249, 231, 159);
+  	}
+    .st1{
+  		width  						: 130px;    	
+  		height   					: 30px;
+  		border    				: none;
+  		background-color  : rgb(163, 228, 215);  		
     }
-
-    .bt{
-      margin-top: 5px;
+    .sto{
+  		width  						: 130px;   	
+    	margin   					: 10px;
+  		height  					: 30px;
+  		border  					: none;
+  		background-color  : rgb(245, 183, 177); 
     }
-
-    .seconds{
-      font-size: 40px;
-      font-family: sans-serif;
-    }
+    .re1{
+  		width  						: 130px;    	
+  		height   					: 30px;
+  		border  					: none;
+  		background-color  : rgb(174, 182, 191); 
+  	}
   </style>
 </head>
 <body>
-  <h1 style="color: lightblue;"> Stopwatch</h1><br>
-  <div id="output"><span><b>0</b> <a>s</a>: <a>00</a></span></div>
-  <div id="myProgress"></div>
-  <div id="myBar"></div>
-  <div class="bt"> 
-    <button id='start' class="btn-primary">Start Time</button>
-    <button id="reset" class="btn-danger">Reset Time</button>
-  </div>
-  <div class="bt"> 
-    <button id='start1' class="btn-success">Start Time</button>
-    <button id='stop1' class="btn-danger">Stop Time</button>
-    <button id="reset1" class="btn-primary">Reset Time</button>
-  </div>
-
-
-<script type="text/javascript">
+	<div class="container">
+		<div class="col-md-12" id="form">
+      <h1 class="text-center" style="color: RGB(250, 128, 114);">Stop Watch With Timer</h1><hr>
+      <div col-lg-9> <button id="watch" class="stopwatch">STOP WATCH</button>&nbsp;<button id="timer" class="timer">TIMER</button></div><hr>
+      <div id="#hi">
+	      <div id="output"><span><b>0</b> <a>s</a>: <a>00</a></span></div>      
+			  <div class="bt"> 
+			    <button id='start' class="st">Start Time</button>&nbsp;
+			    <button id="reset" class="re">Reset Time</button>
+			  </div>
+			  <div class="bt"> 
+			    <button id='start1' class="st1">Start Time</button>
+			    <button id='stop1' class="sto">Stop Time</button>
+			    <button id="reset1" class="re1">Reset Time</button>
+			  </div>
+			</div>
+    </div>
+	</div>
+	<script type="text/javascript">
 
 $(document).ready(function(){
+
+	$("#timer").click(function(){
+    $("#hi").css('display','none');
+	});
+
+	$("#watch").click(function(){
+	  $("#hi").css('display','block');
+	});
 
   var intervalTracker = null; 
 
@@ -116,7 +156,8 @@ $(document).ready(function(){
       // 50 mill Second  =  1/2 Second
 
         $(this).text('Stop Time');
-      }else{ // i.e  $(this).text() == 'Stop Time'
+    }
+      else{ // i.e  $(this).text() == 'Stop Time'
         clearInterval( intervalTracker );
         $(this).text('Start Time');
       }
@@ -188,9 +229,6 @@ $(document).ready(function(){
 
 });
 
-</script>
-
-
-
+</script>	
 </body>
 </html>
